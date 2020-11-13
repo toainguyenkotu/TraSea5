e2c61b0a7831a4533a71063e605f06c93f54087f
KDI EDUCATION <toainguyen232696@gmail.com>
2020-11-13 18:55:29 +0700

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/res/layout/activity_main.xml
    #	app/src/main/res/values/colors.xml

--- app/src/main/java/com/example/trasea/SplashScreenActivity.java
+++ app/src/main/java/com/example/trasea/SplashScreenActivity.java
@@ -1,10 +1,15 @@
 package com.example.trasea;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.animation.Animator;
 import android.animation.AnimatorInflater;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -24,20 +29,34 @@ public class SplashScreenActivity extends AppCompatActivity {
         setContentView(R.layout.activity_splash_screen);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-        tv_welcome = findViewById(R.id.tv_welcome);
-        tv_trase = findViewById(R.id.tv_trase);
-        iv_splash_man_riding = findViewById(R.id.iv_splash_man_riding);
-        iv_splash_progressbar = findViewById(R.id.iv_splash_progressbar);
-        iv_background = findViewById(R.id.iv_background);
+        findViews();
 
+        animateViews();
+
+        final Handler handler = new Handler(Looper.getMainLooper());
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreenActivity.this, LogInActivity.class);
+                startActivity(intent);
+            }
+        }, 4300);
+
+    }
+
+    private void animateViews() {
         iv_background.animate().translationY(-2600).setDuration(700).setStartDelay(4000);
         tv_welcome.animate().translationY(2600).setDuration(700).setStartDelay(4000);
         iv_splash_man_riding.animate().translationY(2600).setDuration(700).setStartDelay(4000);
         iv_splash_progressbar.animate().translationY(2600).setDuration(700).setStartDelay(4000);
         tv_trase.animate().translationY(2600).setDuration(700).setStartDelay(4000);
+    }
 
-
-
-
+    private void findViews() {
+        tv_welcome = findViewById(R.id.tv_welcome);
+        tv_trase = findViewById(R.id.tv_trase);
+        iv_splash_man_riding = findViewById(R.id.iv_splash_man_riding);
+        iv_splash_progressbar = findViewById(R.id.iv_splash_progressbar);
+        iv_background = findViewById(R.id.iv_background);
     }
 }
